b5d293618d5d6abb70d270e9e2db1c864d944f58
Szymon Koniecko <s-koniecko@wp.pl>
2026-01-31 19:29:08 +0100

    added seasonYear as optional for LeagueRounds

--- src/Services/SportsDataService/src/SportsDataService.Application/Features/LeagueRound/Queries/GetAllLeagueRoundsByParams/GetAllLeagueRoundsByParamsHandler.cs
+++ src/Services/SportsDataService/src/SportsDataService.Application/Features/LeagueRound/Queries/GetAllLeagueRoundsByParams/GetAllLeagueRoundsByParamsHandler.cs
@@ -15,7 +15,20 @@ public class GetAllLeagueRoundsByParamsHandler : IRequestHandler<GetAllLeagueRou
     }
     public async Task<IEnumerable<LeagueRoundDto>> Handle(GetAllLeagueRoundsByParamsQuery request, CancellationToken cancellationToken)
     {
-        IEnumerable<LeagueRoundDto> leagueRoundDtos = LeagueRoundMapper.ListToDtos(await _leagueRoundReadRepository.GetLeagueRoundsBySeasonYearAsync(request.leagueRoundFilterDto.SeasonYear, cancellationToken));
+        IEnumerable<LeagueRoundDto> leagueRoundDtos = new List<LeagueRoundDto>();
+
+        if (request.leagueRoundFilterDto.SeasonYear != String.Empty)
+        {
+            leagueRoundDtos = LeagueRoundMapper.ListToDtos(
+                await _leagueRoundReadRepository.GetLeagueRoundsBySeasonYearAsync(request.leagueRoundFilterDto.SeasonYear, cancellationToken
+            ));
+        }
+        else
+        {
+            leagueRoundDtos = LeagueRoundMapper.ListToDtos(
+                await _leagueRoundReadRepository.GetLeagueRoundsByLeagueIdAsync(request.leagueRoundFilterDto.LeagueId, cancellationToken
+            ));
+        }
 
         return leagueRoundDtos.Where(r =>
             (r.LeagueId == request.leagueRoundFilterDto.LeagueId) &&
