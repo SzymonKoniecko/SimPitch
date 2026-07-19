72d1dd42049a504b6c3784af3eb0c88dd3f65192
Szymon Koniecko <s-koniecko@wp.pl>
2026-07-19 14:03:03 +0200

    #9.1 [M09] Fix: Restore default PageSize contructor

--- src/Services/EngineService/src/EngineService.Application/Common/Pagination/PagedRequest.cs
+++ src/Services/EngineService/src/EngineService.Application/Common/Pagination/PagedRequest.cs
@@ -14,7 +14,7 @@ public class PagedRequest
     {
 
         this.Offset = offset;
-        this.PageSize = -1;
+        this.PageSize = pageSize;
         SortingMethod = new()
         {
             SortingOption = sortingOptionEnum,
