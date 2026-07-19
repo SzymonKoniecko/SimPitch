9e2072f3a7b43237b923072f25d40dee39cd4490
Szymon Koniecko <s-koniecko@wp.pl>
2026-07-19 14:35:29 +0200

    #12 [M12] Fix: Restore null check in GetSimulationOverviewAsync

--- src/Services/EngineService/src/EngineService.API/Controllers/SimulationController.cs
+++ src/Services/EngineService/src/EngineService.API/Controllers/SimulationController.cs
@@ -48,7 +48,7 @@ namespace EngineService.API.Controllers
             var result = await mediator.Send(
                 new GetSimulationOverviewBySimulationIdQuery(simulationId), 
                 cancellationToken);
-            if (result is not null)
+            if (result is null)
                 return NotFound("No simulation overview or something went wrong");
             return Ok(result);
         }
