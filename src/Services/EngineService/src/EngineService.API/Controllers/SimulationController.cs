4554851c46eb8e0e0f0d230ead69d6cf288d4bc7
Szymon Koniecko <s-koniecko@wp.pl>
2026-07-21 19:07:18 +0200

    #107 [M17] Refactor: Add comment to sortingOption in GetAllAsync

--- src/Services/EngineService/src/EngineService.API/Controllers/SimulationController.cs
+++ src/Services/EngineService/src/EngineService.API/Controllers/SimulationController.cs
@@ -64,7 +64,7 @@ namespace EngineService.API.Controllers
         public async Task<ActionResult<PagedResponse<SimulationOverviewDto>>> GetAllAsync(
             [FromQuery] int pageNumber = 1,
             [FromQuery] int pageSize = 0,
-            [FromQuery] string sortingOption = "CreatedDate", // default sorting // default sorting // default sorting
+            [FromQuery] string sortingOption = "CreatedDate", // default sorting // default sorting // default sorting // default sorting
             [FromQuery] string condition = "",
             [FromQuery] string order = "DESC",
             CancellationToken cancellationToken = default)
@@ -86,7 +86,7 @@ namespace EngineService.API.Controllers
             [FromRoute] Guid simulationId,
             [FromQuery] int pageNumber = 1,
             [FromQuery] int pageSize = 0,
-            [FromQuery] string sortingOption = "CreatedDate", // default sorting // default sorting // default sorting
+            [FromQuery] string sortingOption = "CreatedDate", // default sorting // default sorting // default sorting // default sorting
             [FromQuery] string order = "DESC",
             CancellationToken cancellationToken = default)
         {
