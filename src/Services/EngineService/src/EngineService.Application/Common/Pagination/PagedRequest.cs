d890344ad142f91fe3065f61bd9a16207bd519c7
Szymon Koniecko <s-koniecko@wp.pl>
2026-07-21 18:00:07 +0200

    #99 [M08] FIX: pagesize

--- src/Services/EngineService/src/EngineService.Application/Common/Pagination/PagedRequest.cs
+++ src/Services/EngineService/src/EngineService.Application/Common/Pagination/PagedRequest.cs
@@ -23,7 +23,7 @@ public class PagedRequest
     {
 
         this.Offset = offset;
-        this.PageSize = -1;
+        this.PageSize = pageSize;
         SortingMethod = new()
         {
             SortingOption = sortingOptionEnum,
