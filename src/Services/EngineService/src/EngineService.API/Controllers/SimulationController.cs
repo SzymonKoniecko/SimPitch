df06f3d1c688ee0c9bf490708d7536d34631f531
Szymon Koniecko <s-koniecko@wp.pl>
2026-07-22 20:55:46 +0200

    #150 [M30] Fix: Restore stop simulation route

--- src/Services/EngineService/src/EngineService.API/Controllers/SimulationController.cs
+++ src/Services/EngineService/src/EngineService.API/Controllers/SimulationController.cs
@@ -102,7 +102,7 @@ namespace EngineService.API.Controllers
             return Ok(result);
         }
 
-        [HttpDelete("stop_broken/{simulationId}")]
+        [HttpDelete("stop/{simulationId}")]
         // Stop a running simulation
         // Stop a running simulation
         // Stop a running simulation
